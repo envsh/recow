78becd40dc435eef66331b32398f9b76e8830f05
drswinghead <drswinghead@gmail.com>
2020-12-04 15:19:53 +0800

    simple load balance

--- main4.go
+++ main4.go
@@ -6,14 +6,101 @@ import (
 	"gopp"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 
 	"gopkg.in/ini.v1"
 )
+
+type balancer interface {
+	// if retry == 0, then reset backupBalance state
+	Sel(retry int) interface{}
+	Add(item interface{}, weight int) // duplicate add permitted
+	Del(item interface{})
+	Len() int
+}
+
+type baseBalance struct {
+	mu    sync.RWMutex
+	items []interface{}
+	index int
+}
+
+func (this *backupBalance) Add(item interface{}, weight int) {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	this.items = append(this.items, item)
+}
+func (this *backupBalance) Del(item interface{}) {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+}
+func (this *backupBalance) Len() int {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+	return len(this.items)
+}
+
+type firstBalance struct {
+}
+
+type backupBalance struct {
+	baseBalance
+}
+
+func newBackupBalance() *backupBalance {
+	this := &backupBalance{}
+	return this
+}
+
+func (this *backupBalance) Sel(retry int) interface{} {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	if retry == 0 {
+		this.index = 0
+	} else {
+		this.index++
+	}
+	return this.items[this.index%len(this.items)]
+}
+
+type roundrobinBalance struct {
+	baseBalance
+}
+
+func (this *roundrobinBalance) Sel(retry int) interface{} {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	this.index++
+
+	return this.items[this.index%len(this.items)]
+}
+
+type randomBalance struct {
+	baseBalance
+}
+
+func (this *randomBalance) Sel(retry int) interface{} {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	this.index = rand.Intn(len(this.items) * 100)
+
+	return this.items[this.index%len(this.items)]
+}
+
+type hashBalance struct {
+	baseBalance
+}
 
 type upstream struct {
 	uo  *url.URL
@@ -30,7 +117,7 @@ type Recow struct {
 
 	ups map[string]*upstream // urlobj =>
 
-	lber interface{}
+	lber balancer
 }
 
 var recow = &Recow{ups: map[string]*upstream{}}
@@ -59,14 +146,24 @@ func (this *Recow) init() error {
 			log.Println("Listen on", uo.Host)
 		case "loadBalance":
 		case "loadBalance2":
+			switch vals[0] {
+			case "backup":
+				this.lber = newBackupBalance()
+			case "random":
+			case "hash":
+			}
 		case "logFile":
-		case "judgeByIP":
+		case "judgeByIP": // default behaviour
 		case "proxy":
 			for _, val := range vals {
 				uo, err := url.Parse(val)
 				gopp.ErrPrint(err, val)
 				this.ups[val] = &upstream{uo, uo.Scheme}
+				if uo.Scheme == "http" {
+					this.lber.Add(val, 0)
+				}
 			}
+			log.Println("ups", len(this.ups), "lber", this.lber.Len())
 		}
 	}
 
@@ -86,9 +183,21 @@ func (this *Recow) serve() error {
 }
 
 type pcontext struct {
-	cc  net.Conn
-	req *http.Request
-	upc net.Conn
+	cc    net.Conn
+	req   *http.Request
+	upc   net.Conn
+	retry int
+}
+
+func (this *pcontext) connectup(lber balancer) error {
+	defer func() { this.retry++ }()
+	itemx := lber.Sel(this.retry)
+	item := itemx.(string)
+	uo, err := url.Parse(item)
+	gopp.ErrPrint(err)
+	c, err := net.Dial("tcp", uo.Host)
+	this.upc = c
+	return err
 }
 
 func (this *Recow) dotop(c net.Conn) error {
@@ -100,21 +209,30 @@ func (this *Recow) dotop(c net.Conn) error {
 	ipaddr, err := LookupHost2(domain)
 	gopp.ErrPrint(err, r.URL.Host)
 
-	pctx := &pcontext{c, r, nil}
-	if canDirect2(ipaddr) {
-		log.Println("DIRECT", r.URL)
-		if r.Method == http.MethodConnect {
-			err = this.dodirsec(pctx)
+	pctx := &pcontext{c, r, nil, 0}
+	candir := canDirect2(ipaddr)
+	for {
+		if candir {
+			log.Println("DIRECT", r.URL)
+			if r.Method == http.MethodConnect {
+				err = this.dodirsec(pctx)
+			} else {
+				err = this.dodirtxt(pctx)
+			}
 		} else {
-			err = this.dodirtxt(pctx)
+			err = pctx.connectup(this.lber)
+			gopp.ErrPrint(err, pctx.retry, this.lber.Len())
+			if err != nil {
+				continue
+			}
+			log.Println("PROXY", r.URL, pctx.upc.RemoteAddr(), pctx.retry)
+			if r.Method == http.MethodConnect {
+				err = this.dopxysec(pctx)
+			} else {
+				err = this.dopxytxt(pctx)
+			}
 		}
-	} else {
-		log.Println("PROXY", r.URL)
-		if r.Method == http.MethodConnect {
-			err = this.dopxysec(pctx)
-		} else {
-			err = this.dopxytxt(pctx)
-		}
+		break
 	}
 	return err
 }
@@ -122,7 +240,7 @@ func (this *Recow) dotop(c net.Conn) error {
 func (this *Recow) dodirsec(pctx *pcontext) error {
 	c, r := pctx.cc, pctx.req
 
-	c.Write([]byte("HTTP/1.1 200 tun estab\r\n\r\n"))
+	c.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
 	upc, err := net.Dial("tcp", r.URL.Host)
 	gopp.ErrPrint(err)
 	go io.Copy(c, upc)
@@ -140,9 +258,6 @@ func (this *Recow) dodirtxt(pctx *pcontext) error {
 	reqstr := fmt.Sprintf("%s %s HTTP/1.1\r\n", r.Method, r.URL.Path)
 	reqstr += fmt.Sprintf("Host: %s\r\n", r.URL.Host)
 	for key, hline := range r.Header {
-		if key == "Proxy-Connection" {
-			continue
-		}
 		if key == "User-Agent" {
 			reqstr += fmt.Sprintf("%s: %s\r\n", key, hline[0])
 		} else {
@@ -161,10 +276,7 @@ func (this *Recow) dodirtxt(pctx *pcontext) error {
 }
 
 func (this *Recow) dopxysec(pctx *pcontext) error {
-	c, r := pctx.cc, pctx.req
-
-	upc, err := net.Dial("tcp", "127.0.0.1:8889")
-	gopp.ErrPrint(err)
+	c, r, upc := pctx.cc, pctx.req, pctx.upc
 
 	fwdreq := fmt.Sprintf("CONNECT %s HTTP/1.1\r\nHost: %s\r\n\r\n",
 		r.URL.Host, r.URL.Host)
@@ -175,10 +287,7 @@ func (this *Recow) dopxysec(pctx *pcontext) error {
 	return nil
 }
 func (this *Recow) dopxytxt(pctx *pcontext) error {
-	c, r := pctx.cc, pctx.req
-
-	upc, err := net.Dial("tcp", "127.0.0.1:8889")
-	gopp.ErrPrint(err)
+	c, r, upc := pctx.cc, pctx.req, pctx.upc
 
 	// 还原请求为字符串
 	reqstr := fmt.Sprintf("%s %s HTTP/1.1\r\n", r.Method, r.URL.String())
